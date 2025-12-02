49acbca50c6a3e78956706183f8fba4472254c4c
Abdulwahab Abbas <abbas@seamlesshr.com>
2025-12-02 23:31:41 +0100

    done

--- src/routes/_appLayout/formula/summary.$formularId.lazy.tsx
+++ src/routes/_appLayout/formula/summary.$formularId.lazy.tsx
@@ -124,7 +124,10 @@ function FormularSummaryComponent() {
           <span className="">COGS & Formula Summary</span>
         </button>
         {lastCreatedMarkettingInfo && (
-          <button className="flex items-center gap-2 bg-[#F4DD5F] hover:bg-yellow-500 text-black px-4 py-2 rounded-full font-medium">
+          <button
+            className="flex items-center gap-2 bg-[#F4DD5F] hover:bg-yellow-500 text-black px-4 py-2 rounded-full font-medium"
+            disabled
+          >
             <Download size={18} />
             Export to Brief
           </button>
